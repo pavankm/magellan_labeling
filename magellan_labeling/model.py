c017cbb2c9c96ae8b0c3e582250c776bf5cc51a0
Mushahid Alam <mushahid@cs.wisc.edu>
2016-02-19 01:52:32 -0600

    Return Dataframes of type label passed to model.py

--- magellan_labeling/model.py
+++ magellan_labeling/model.py
@@ -1,2 +1,7 @@
-def get_label():
-    pass
+import pandas as pd
+import numexpr
+def get_label(label):
+    df = pd.read_csv("data/sample.csv")
+    Result_DataFrame = pd.DataFrame()
+    Result_DataFrame=df[df.label=="yes"]
+    return Result_DataFrame
