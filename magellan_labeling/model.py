80249544837d1b8e1ac9766adc45cb5a8999dee0
Mushahid Alam <mushahid6666@gmail.com>
2016-02-21 16:41:12 -0600

    changed variable name

--- magellan_labeling/model.py
+++ magellan_labeling/model.py
@@ -3,6 +3,5 @@ import pandas as pd
 
 def get_label():
     df = pd.read_csv("data/sample.csv")
-    Result_DataFrame=df.label
-    # print(Result_DataFrame)
-    return Result_DataFrame
+    result_dataframe=df.label
+    return result_dataframe
