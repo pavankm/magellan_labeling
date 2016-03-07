d5a02261beb286dc607c58e75fb6865a139d2175
Mushahid Alam <mushahid@cs.wisc.edu>
2016-03-07 00:31:02 -0600

    Updated 1] get_rows_label of model returns the rows with label 2]get_possible_label of model returns possibles label values

--- magellan_labeling/controller.py
+++ magellan_labeling/controller.py
@@ -1,15 +1,26 @@
-import model
+from model import ModelClass
 import pandas as pd
-def get_summary():
-    result_df=model.get_label()
-    labels_list = result_df.label
-    labels_count = {}
-    for label in labels_list:
-        if label in labels_count:
-            labels_count[label]=labels_count[label]+1
-        else:
-            labels_count[label]=1
-    df = pd.DataFrame(pd.Series(labels_count),columns=list('c'))
-    return (df)
 
-print get_summary()
+class ControllerClass():
+    label_count_dict= dict()
+    model = ModelClass()
+    def get_summary(self):
+        self.set_labels()
+        result_df=self.model.get_label()
+        labels_list = result_df.label
+        for label in labels_list:
+            self.label_count_dict[label]=self.label_count_dict[label]+1
+        df = pd.DataFrame(pd.Series(self.label_count_dict),columns=list('c'))
+        return (df)
+
+    def set_labels(self):
+        possible_labels = self.model.get_possible_label()
+        for label in possible_labels:
+            self.label_count_dict[label]=0
+
+    def get_rows(self,label):
+        return self.model.get_rows_label([label])
+
+obj = ControllerClass()
+print obj.get_summary()
+print obj.get_rows('no')
--- magellan_labeling/model.py
+++ magellan_labeling/model.py
@@ -1,8 +1,20 @@
 __author__='MushahidAlam'
 import pandas as pd
 
-def get_label():
+class ModelClass():
     df = pd.read_csv("data/sample.csv")
-    result_dataframe=df.label
-    df = pd.DataFrame(pd.Series(result_dataframe))
-    return df
+    def get_label(self):
+        result_dataframe=self.df.label
+        label_df = pd.DataFrame(pd.Series(result_dataframe))
+        return label_df
+
+    def get_possible_label(self):
+        label_coulumn= self.df.label.values
+        labels=[]
+        for label in label_coulumn:
+            if label not in labels:
+                labels.append(label)
+        return labels
+
+    def get_rows_label(self,label):
+        return self.df[self.df['label'].isin(label)]
