c6ccdf7d4afdc74527bfb6a5b85660c58570e4c8
Mushahid Alam <mushahid@cs.wisc.edu>
2016-03-15 15:51:00 -0500

    Working:  Label Given list of ltable,rtable,label

--- magellan_labeling/controller.py
+++ magellan_labeling/controller.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 label_count_dict= dict()
 def get_summary():
+    # remove this
     set_labels()
     result_df=model.get_label()
     labels_list = result_df.label
@@ -14,25 +15,39 @@ def get_summary():
 def set_labels():
     possible_labels = model.get_possible_label()
     for label in possible_labels:
-        label_count_dict[label]=0
+        label_count_dict[label] = 0
 
 def get_rows(labels):
-
     for label in labels:
         if label not in label_count_dict:
             print 'WARNING: Not a valid label',label
             return False
-    return model.get_rows_given_label(labels)
+    return model.get_rows_for_given_label_list(labels)
 
 
-def update_labels(input_list):
+def update_labels_for_tuple_pairs(input_list):
     result = False
     for row in input_list:
         if len(row) !=3:
+            # logging, check for length and type
             print 'WARNING: Input [<ltable.id>,<rtable.id>,<label>] all 3 arguments should be string'
+
+        # @todo think about where the valid label checking should be done !!!
+        # The reason is in get_rows we do the label checking at controller level.
+        # but here only partial check can be done. ltable.ID and rtable.ID checking are done at model level.
+
+        # To be consistent, we can do the label checking at the model level.
+
+        # Anything that is sent to model, that the model knows about it is checked by the model.
+
+        # Anything that is returned by the model and the controller processes them for the higher layers should be
+        # checked/done at the controller level.
+
         if row[2] not in label_count_dict:
             print 'WARNING: Not a valid label'
-        if model.update_labels(row):
+
+
+        if model.update_label_for_a_tuple_pair(row):
             result = True
     return result
 
--- magellan_labeling/model.py
+++ magellan_labeling/model.py
@@ -5,23 +5,29 @@ import numpy as np
 df = pd.read_csv("../magellan_labeling/data/sample.csv")
 def get_label():
     result_dataframe=df.label
-    label_df = pd.DataFrame(pd.Series(result_dataframe))
+    # print type(result_dataframe)
+    label_df = pd.DataFrame(result_dataframe)
     return label_df
 
 def get_possible_label():
-    labels=['no','not-sure','unlabeled','yes']
+    #@todo the valid labels should be in a config value.
+    labels=['no','not-sure','unlabeled','yes'] # unlabeled,yes, no, not-sure
     return labels
 
-def get_rows_given_label(label):
-    return df[df['label'].isin(label)]
+def get_rows_for_given_label_list(label_list):
+    #@todo have a contract to get columnname with labels.
+    return df[df['label'].isin(label_list)]
 
-def update_labels(input_list):
-    if any(df['ltable.ID']==input_list[0]):
-        if any(df['rtable.ID']==input_list[1]):
-            df.loc[(df['ltable.ID']==input_list[0]) & (df['rtable.ID']==input_list[1]),'label']=input_list[2]
+def update_label_for_a_tuple_pair(tuple_pair):
+    #@todo have a contract to get columnname with ltable.ID, rtable.ID.
+    if any(df['ltable.ID']==tuple_pair[0]):
+        if any(df['rtable.ID']==tuple_pair[1]):
+            df.loc[(df['ltable.ID']==tuple_pair[0]) & (df['rtable.ID']==tuple_pair[1]),'label']=tuple_pair[2]
+            # Update this with absolute path
             df.to_csv("data/sample.csv",mode = 'w', index=False)
             return True
         else:
+            #@todo use loggers!!!!
             print "WARNING: rtable.ID doesn't exist"
             return False
     else:
@@ -29,6 +35,7 @@ def update_labels(input_list):
         return False
 
 
+# print get_label()
 #Testing the code for update_labels
 # input_list = ['a2','b3','new_test']
 # print update_labels(input_list)
